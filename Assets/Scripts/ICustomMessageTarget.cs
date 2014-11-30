521e37b2fd586c5b035dbf8cb9e890f258ad57a6
moe-ped <m.berning@web.de>
2014-11-30 23:11:27 +0100

    Initial commit

---
+++ Assets/Scripts/ICustomMessageTarget.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+
+public interface ICustomMessageTarget : IEventSystemHandler
+{
+	// functions that can be called via the messaging system
+	void OnStartGame();
+}
---
+++ Assets/Scripts/SubtitleSelector.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+// ahaaa!
+using UnityEngine.UI;
+
+public class SubtitleSelector : MonoBehaviour, ICustomMessageTarget 
+{
+
+	public string[] phrases;
+
+	// Use this for initialization
+	void Start () 
+	{
+		selectSubtitle ();
+	}
+
+	public void OnStartGame () 
+	{
+		// test
+		selectSubtitle ();
+	}
+
+	void selectSubtitle ()
+	{
+		// randomly pick 2 different phrases
+		if (phrases.Length >= 2)
+		{
+			int phrase1 = Random.Range (0, phrases.Length);
+			int phrase2 = phrase1;
+			while (phrase2 == phrase1) {
+				phrase2 = Random.Range (0, phrases.Length);
+			}
+			Text textComponent = GetComponent<Text>();
+			textComponent.text = "Memoirs of " + phrases[phrase1] + " and " + phrases[phrase2];
+		}
+	}
+}
